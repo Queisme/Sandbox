ad07a538b935e59de95f44e7cf978d0774ceb070
Queisme <queiskey@gmail.com>
2024-08-20 09:00:00 -0500

    Python update

---
+++ Python/12 hour Python full course/hello world/main.py
@@ -0,0 +1,76 @@
+# print('I love pizza')
+# print('It is really good')
+# print('Welcome to coding with Python')
+
+#variables
+
+# firstName = 'Que '
+# lastName = 'Code'
+# fullName = firstName + lastName
+
+# print(f'Hello {fullName}')
+# print(type(name))
+# print(f'Hello {name}')
+# print("Hello " + name)
+
+# age = 30
+# age += 1
+
+# print(f'You are {age} years old')
+# print(type(age))
+
+# height = 250.5
+
+# print(f'The height is {height} cms')
+# print(height)
+# print(type(height))
+
+# human = False
+
+# print(f'Are you a human? {human}')
+# print(type(human))
+# print(human)
+
+# multiple assignment = allows us to assign multiple cariables at the same time in one line of code
+
+# standard assignment
+# name = 'Que'
+# age = 1110
+# attractive = False
+
+# name, age, attractive = 'Que', 1110, False
+
+# print(name)
+# print(age)
+# print(attractive)
+
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+# Spongebob = Patrick = Sandy = Squidward = 30
+
+# print(Spongebob)
+# print(Patrick)
+# print(Sandy)
+# print(Squidward)
+
+# String Methods
+
+# name = 'que'
+
+# print(len(name)) #length
+# print(name.find('e')) #gives first index
+# print(name.capitalize()) #capitalizes first letter
+# print(name.upper()) #capitalizes whole word
+# print(name.lower()) #lowercases whole word
+# print(name.isdigit()) #bool for if it's a number
+# print(name.isalpha()) #bool for it it's letters
+# print(name.count('e')) #counts the amount of letter you determine
+# print(name.replace('e', 'i')) #replaces a letter with what you determine
+# print(name*3) #repeats name however many times you determine
+
+# type casting = converting data type of values to another data type
+# str(), int(), float(), bool()
+
